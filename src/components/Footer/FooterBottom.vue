cacf8d7c4bf4ed1794e723f1f6d33882bb3df2f8
Matteo Morganti <matteo99morganti@gmail.com>
2022-02-21 18:28:23 +0100

    Footer bottom metà

---
+++ src/components/Footer/FooterBottom.vue
@@ -0,0 +1,76 @@
+<template>
+    <div id="footer-bottom">
+        <div class="container d-flex d-center d-justify-between">
+            <div id="sign-up"><a href="#">SIGN-UP NOW!</a></div>
+            <div id="social">
+                <h3>FOLLOW US</h3>
+                <ul>
+                    <li class="debug"></li>
+                    <li class="debug"></li>
+                    <li class="debug"></li>
+                    <li class="debug"></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'FooterBottom',
+    }
+</script>
+
+
+<style lang="scss" scoped>
+
+    @import "../../assets/style/general.scss";
+    @import "../../assets/style/utils.scss";
+    @import "../../assets/style/variables.scss";
+
+
+    .container{
+        padding: 20px 0;
+    }
+
+    #footer-bottom{
+        background-color: #303030;
+
+        #sign-up{
+            a{
+                color: #fff;
+                padding: 15px;
+                text-decoration: none;
+                border: 2px solid $secondaryColor;
+            }
+        }
+    }
+
+    #social{
+        display: flex;
+        align-items: center;
+
+        h3{
+            color: $secondaryColor;
+        }
+
+        ul{
+            display: inline-block;
+            list-style-type: none;
+            display: flex;
+        }
+
+        li{
+            margin: 0 10px;
+        }
+
+        .debug{
+            height: 30px;
+            width: 30px;
+            border-radius: 50%;
+            background-color: blue;
+        }
+    }
+
+</style>
--- src/components/FooterComponents.vue
+++ src/components/FooterComponents.vue
@@ -1,17 +1,21 @@
 <template>
-   
-    <FooterTop />
-
+   <footer>
+        <FooterTop />
+        <FooterBottom />
+   </footer>
+ 
 </template>
 
 
 <script>
     import FooterTop from './Footer/FooterTop.vue';
+    import FooterBottom from './Footer/FooterBottom.vue';
     
     export default {
         name: 'FooterComponents',
         components: {
             FooterTop,
+            FooterBottom
         }
     }
 </script>
